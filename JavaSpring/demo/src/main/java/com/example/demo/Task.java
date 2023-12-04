a819e871e2efb798591902d6fde7a3c76a848a37
Maximiliano Cassol Montagner <maximilianocassol@gmail.com>
2023-12-04 11:42:20 -0300

    java spring

---
+++ JavaSpring/demo/src/main/java/com/example/demo/Task.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import java.util.Date;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private Date dueDate;
+
+    // Constructor, getters y setters
+}
