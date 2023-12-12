fba41e404433f5442b9cf3b2fd6071ce96266b56
Maximiliano Cassol Montagner <maximilianocassol@gmail.com>
2023-12-12 17:57:18 -0300

    restful api

---
+++ ApiRESTfulProductos/src/main/java/com/ApiRESTful/ApiRESTfulProductos/ApiResTfulProductosApplication.java
@@ -0,0 +1,13 @@
+package com.ApiRESTful.ApiRESTfulProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfulProductosApplication.class, args);
+	}
+
+}
---
+++ ApiRESTfulProductos/src/test/java/com/ApiRESTful/ApiRESTfulProductos/ApiResTfulProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiRESTful.ApiRESTfulProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfulProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
